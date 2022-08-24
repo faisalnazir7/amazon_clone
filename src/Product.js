47f1ee26bb870c6804e4d4556d6b709b70dc4030
faisalnazir7 <bhatfaisalfnb@gmail.com>
2022-08-24 14:42:06 +0530

    datalayer

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import "./Product.css";
+import { useStateValue } from './StateProvider';
 
 function Product
 ({ title, image, price, rating }) {
+
+
+    const [{basket}, dispatch] = useStateValue();
+
+    console.log("this is basket", basket);
+
+    const addToBasket = () => {
+      // dispatch item to data layer
+      dispatch({
+        type: "ADD_TO_BASKET",
+        item: {
+          id: id,
+          title: title,
+          image: image,
+          price: price,
+          rating: rating,
+        },
+      });
+    };
+
   return (
     <div className='product'>
         <div className='product__info'>
@@ -20,7 +41,7 @@ function Product
             </div>
         </div>
         <img src='https://unsplash.com/s/photos/water-bottle'/>
-        <button>Add to Cart </button>
+        <button onClick={addToBasket}>Add to Cart </button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { StateProvider } from './StateProvider';
+import * as ServiceWorker from "./serviceWorker";
+import reducer, { initialState } from "./reducer";
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+    <App />
+    </StateProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+ServiceWorker.unregister();
