9c2057c200ef43b8417d97f87c7076bfd39fee78
faisalnazir7 <bhatfaisalfnb@gmail.com>
2022-09-13 21:23:43 +0530

    authState unfnished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from'./Home';
 import Checkout from './Checkout';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './Login';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useStateValue } from './StateProvider';
+import {auth} from "./firebase"
 
 class App extends Component {
   render() {
+
+    const [{}, dispatch] = useStateValue();
+
+    useEffect(() => {
+      // runs once app components runs
+      
+      onAuthStateChanged(auth, (authUser) => {
+        console.log("the user is >>> ",authUser);
+        if (authUser) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          // const uid = user.uid;
+          dispatch({
+            type: ' SET_USER',
+            user: authUser
+          })
+          // ...
+        } else {
+          // User is signed out
+          dispatch({
+            type: 'SET_USER',
+            user: authUser
+          })
+          // ...
+        }
+      });
+    }, [])
+
     return (
 
       //BEM convention
